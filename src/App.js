b3b4ef28e75ae4be338caa10b9672fc8239b45ef
Cody Tomesch <tomeschcody@gmail.com>
2018-10-17 12:31:42 -0600

    increased map size and apparently did everything else

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ class App extends Component {
   constructor() {
     super() 
     this.state = {
-      map: maps.five.map,
+      map: maps.beginner.map,
       mines: [],
       dead: false,
+      numMine: maps.beginner.mines
     }
   }
   componentDidMount() {
+    this.setMines(this.state.map, this.state.numMine)
+  }
+
+  setMines = (mapStart, numMines) => {
     let mines = []
-    let map = this.state.map.slice()
-    for(let i = 0; i < map.length; i++) {
+    let map = mapStart.slice()
+    for(let i = 0; i < numMines; i++) {
       let mine = []
       let flag = true
       while(flag) {
@@ -58,23 +63,67 @@ class App extends Component {
         }
       }
     }
-    console.log(map)
-
-
-    this.setState({mines, map})
+    this.setState({mines, map, dead: false})
   }
 
   handleClick = (f, i) => {
-    let {map} = this.state
+    let map = this.state.map.slice()
+    let points = {
+      tl: [f - 1, i - 1],
+      tc: [f - 1, i],
+      tr: [f - 1, i + 1],
+      ml: [f, i - 1],
+      mr: [f, i + 1],
+      bl: [f + 1, i - 1],
+      bc: [f + 1, i],
+      br: [f + 1, i + 1],
+    }
     if(map[f][i] === '*') {
       this.setState({dead: true})
       alert('You Done Fucked Up!!!!')
-    } else if(map[f][i] === 0) {
-      
+    } else if(map[f][i] > 0) {
+      map[f][i] += 10
+      this.setState({map})
+    } else if (map[f][i] === 0){
+      map[f][i] += 10
+      console.log('prop', map)
+      map = this.updateMap(f, i, map)
+      this.setState({map})
     }
   }
 
+  updateMap = (f, i, map) => {
+    let points = {
+      tl: [f - 1, i - 1],
+      tc: [f - 1, i],
+      tr: [f - 1, i + 1],
+      ml: [f, i - 1],
+      mr: [f, i + 1],
+      bl: [f + 1, i - 1],
+      bc: [f + 1, i],
+      br: [f + 1, i + 1],
+    }
+    for(let prop in points) {
+      if(points[prop][0] >= 0 && points[prop][0] < map.length && points[prop][1] >= 0 && points[prop][1] < map.length ) {
+        if(map[points[prop][0]][points[prop][1]] > 0 && map[points[prop][0]][points[prop][1]] < 10){
+          map[points[prop][0]][points[prop][1]] += 10
+        } else if(map[points[prop][0]][points[prop][1]] === 0 ) {
+          map[points[prop][0]][points[prop][1]] += 10
+          map = this.updateMap(...points[prop], map)
+        }
+      }
+    }
+    return map
+  }
+
+  handleChange = (e) => {
+    let {value} = e.target
+    this.setMines(maps[value].map, maps[value].mines)
+  }
+
   render() {
+    console.log(this.state.map)
+    let count = 0
     let map = this.state.map.map((r, f) => {
       let row = r.map((c, i) => {
         if(this.state.dead){
@@ -84,15 +133,24 @@ class App extends Component {
             )
           } else {
               return(
-                <div onClick={() => this.handleClick(f, i)} className='cube' style={{backgroundColor: 'black'}} key={i}></div>
+                <div className='cube' style={{backgroundColor: 'black'}} key={i}></div>
               )
           }
-
+        } else if(c > 10){
+          count++
+            return(
+              <div className='cube' style={{backgroundColor: 'white', color: 'black'}} key={i}>{c - 10}</div>
+            )
+        } else if (c === 10) {
+          count++
+          return (
+            <div className='cube' style={{backgroundColor: 'white'}} key={i}></div>
+          )
         } else {
+          if(c === '*') count++
           return(
             <div onClick={() => this.handleClick(f, i)} className='cube' style={{backgroundColor: 'black'}} key={i}></div>
           )
-
         }
       })
       return (
@@ -101,10 +159,21 @@ class App extends Component {
         </div>
       )
     })
+    console.log(count)
+    if(count === this.state.map.length * this.state.map.length){
+      alert('You did it!!!!!')
+    }
     return (
-      <div style={{height: '100vh', width: '100vw', outline: 'none', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundColor: 'purple'}} tabIndex="0">
-        {map}
-      </div>
+      // <div style={{height: '100vh', width: '100vw'}}>
+        <div style={{height: '100vh', width: '100vw', outline: 'none', display: 'flex', flexDirection: 'column', justifyContent: 'center', backgroundColor: 'purple'}} tabIndex="0">
+        <select onChange={this.handleChange}>
+          <option value='beginner'>Beginner</option>
+          <option value='intermediate'>Intermediate</option>
+          <option value='expert'>Expert</option>
+        </select>
+          {map}
+        </div>
+      // </div>
     );
   }
 }
